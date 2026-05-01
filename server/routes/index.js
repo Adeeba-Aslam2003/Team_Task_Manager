69691ecbc0e875b193c9af8f532390760e77576a
Adeeba Aslam <adeeba.learnings@gmail.com>
2026-05-01 23:01:10 +0530

    fix: railway deploy path

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,13 @@
 import express from "express";
-import userRoute from "./userRoute.js";
-import taskRoute from "./taskRoute.js";
-
-
+import userRoutes from "./userRoute.js";
+import taskRoutes from "./taskRoute.js";
 
 const router = express.Router();
 
-// test route
 router.get("/", (req, res) => {
   res.send("API is working 🚀");
 });
 
-// 👇 THIS IS MAIN FIX
 router.use("/users", userRoutes);
 router.use("/tasks", taskRoutes);
 
