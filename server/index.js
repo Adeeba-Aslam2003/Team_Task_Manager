e179906609edda1ae509899d8b90fec1a259363c
Adeeba Aslam <adeeba.learnings@gmail.com>
2026-05-01 15:22:18 +0530

    fix duplicate code

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-import User from "./models/userModel.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -18,7 +17,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://teamtaskify.netlify.app"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://teamtaskify.netlify.app",
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
@@ -30,53 +33,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(morgan("dev"));
-
-app.use(morgan("dev"));
-
-// 🔥 YAHI ADD KARNA HAI
-import User from "./models/userModel.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import morgan from "morgan";
-import { errorHandler, routeNotFound } from "./middleware/errorMiddleware.js";
-import routes from "./routes/index.js";
-import dbConnection from "./utils/connectDB.js";
-
-dotenv.config();
-
-dbConnection();
-
-const port = process.env.PORT || 5000;
-
-const app = express();
-
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://teamtaskify.netlify.app"],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser());
-
-app.use(morgan("dev"));
-app.use("/api", routes);
-
-app.use(routeNotFound);
-app.use(errorHandler);
-
-app.listen(port, () => console.log(`Server listening on ${port}`));
-
-
-
-
-// existing route
 
 app.use("/api", routes);
 
